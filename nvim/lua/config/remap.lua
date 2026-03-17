c420e14b34db09cb22056a14d0c71ffda253ee88
michalwachowicz <wachowiczmichal1@gmail.com>
2026-03-17 16:33:50 +0100

    chore: cleanup omnisharp

--- nvim/lua/config/remap.lua
+++ nvim/lua/config/remap.lua
@@ -21,27 +21,7 @@ vim.keymap.set("x", "<leader>p", "\"_dP")
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
 -- LSP --
-local function has_omnisharp_attached()
-	for _, client in ipairs(vim.lsp.get_clients({ bufnr = 0 })) do
-		if client.name == "omnisharp" or client.name == "omnisharp_mono" then
-			return true
-		end
-	end
-
-	return false
-end
-
-vim.keymap.set("n", "gd", function()
-	if has_omnisharp_attached() then
-		local ok, omnisharp_extended = pcall(require, "omnisharp_extended")
-		if ok then
-			omnisharp_extended.telescope_lsp_definition({ jump_type = "never" })
-			return
-		end
-	end
-
-	telescope.lsp_definitions()
-end)
+vim.keymap.set("n", "gd", telescope.lsp_definitions)
 vim.keymap.set("n", "gD", vim.lsp.buf.declaration)
 vim.keymap.set("n", "gi", vim.lsp.buf.implementation)
 vim.keymap.set("n", "gr", vim.lsp.buf.references)
